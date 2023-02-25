2e85e08d893fe961d973aed29ab54ac3fab17d6a
Olivier Paulo <op.olivierpaulo@gmail.com>
2023-02-25 17:39:29 +0100

    New commit for Refactoring

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -2,70 +2,76 @@
 
 from pathlib import Path
 from typing import List, Dict
-import sys
+import argparse
 import pandas as pd
 
-DIR_PATH = f'{Path(__file__).parent}/data'
+DIR_PATH = f"{Path(__file__).parent}/data"
 
 
-def load_data(file_path: str = f'{DIR_PATH}/eu_life_expectancy_raw.tsv') \
-        -> pd.DataFrame:
+def load_data(
+    file_path: str = f"{DIR_PATH}/eu_life_expectancy_raw.tsv",
+) -> pd.DataFrame:
     """Load data from file and Return a Pandas DataFrame"""
-    return pd.read_csv(Path(file_path), sep='\t')
+    return pd.read_csv(Path(file_path), sep="\t")
 
 
-def apply_unpivot(data_frame: pd.DataFrame) -> pd.DataFrame:
+def _apply_unpivot(data_frame: pd.DataFrame) -> pd.DataFrame:
     """Return Dataframe with the unpivots dates and desired columns"""
     id_vars = data_frame.columns[0]
-    col_names = ['unit', 'sex', 'age', 'region', 'year', 'value']
-    unpivot_df = pd.melt(
-                        frame=data_frame,
-                        id_vars=id_vars
-                    )
-    unpivot_df[id_vars.split(',')] = unpivot_df[id_vars].str \
-                                                        .split(',', expand=True)
-    unpivot_df[col_names] = pd.concat([unpivot_df[id_vars.split(',')],
-                                       unpivot_df[['variable', 'value']]], axis=1)
+    col_names = ["unit", "sex", "age", "region", "year", "value"]
+    unpivot_df = pd.melt(frame=data_frame, id_vars=id_vars)
+    unpivot_df[id_vars.split(",")] = unpivot_df[id_vars].str.split(",", expand=True)
+    unpivot_df[col_names] = pd.concat(
+        [unpivot_df[id_vars.split(",")], unpivot_df[["variable", "value"]]], axis=1
+    )
     return unpivot_df[col_names]
 
 
-def apply_data_types(data_frame: pd.DataFrame) -> pd.DataFrame:
+def _apply_data_types(data_frame: pd.DataFrame) -> pd.DataFrame:
     """Ensure data types defined by type_rules
-       Remove NaNs for requested cols"""
-    types_rules: Dict[str, object] = {'year': int, 'value': float}
-    cols_to_delete:  List[str] = ['value']
+    Remove NaNs for requested cols"""
+    types_rules: Dict[str, object] = {"year": int, "value": float}
+    cols_to_delete: List[str] = ["value"]
     for column, data_type in types_rules.items():
         data_frame[column] = pd.to_numeric(
-                                data_frame[column].str.extractall(r'(\d+.\d+)')
-                                                      .astype(data_type)
-                                                      .unstack()
-                                                      .max(axis=1),
-                                errors='coerce')
+            data_frame[column]
+            .str.extractall(r"(\d+.\d+)")
+            .astype(data_type)
+            .unstack()
+            .max(axis=1),
+            errors="coerce",
+        )
     return data_frame.dropna(subset=cols_to_delete)
 
 
-def clean_data(region: str = 'PT',
-               data_frame: pd.DataFrame = load_data()) -> None:
+def clean_data(data_frame: pd.DataFrame, country: str) -> pd.DataFrame:
     """Main function to Clean Data and Filter Region"""
-    clean_df = apply_data_types(apply_unpivot(data_frame))
-    if sys.argv and len(sys.argv) > 1:
-        filter_df = clean_df[clean_df.region.str.upper() == sys.argv[1].upper()]
-    filter_df = clean_df[clean_df.region.str.upper() == region.upper()]
-    save_data(filter_df)
-    return filter_df
+    clean_df = _apply_data_types(_apply_unpivot(data_frame))
+    return clean_df[clean_df.region.str.upper() == country.upper()]
 
 
-def save_data(data_frame: pd.DataFrame) -> None:
+def _save_data(data_frame: pd.DataFrame, country: str = "pt") -> None:
     """Function that saves the data into a local CSV file"""
-    data_frame.to_csv(Path(f'{DIR_PATH}/pt_life_expectancy.csv'), index=False)
+    data_frame.to_csv(Path(f"{DIR_PATH}/{country}_life_expectancy.csv"), index=False)
 
 
-def main() -> None:
+def main(country: str = "PT") -> None:
     """Main Function which call functions of the data pipeline"""
     data_frame = load_data()
-    data_frame = clean_data("PT", data_frame)
-    save_data(data_frame)
+    data_frame = clean_data(data_frame, country)
+    _save_data(data_frame)
 
 
 if __name__ == "__main__":  # pragma: no cover
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-R",
+        "--regions",
+        help="Choose the region(s) you want to filter. Example : cleaning.py -R PT,US,FR",
+    )
+    args = parser.parse_args()
+    if args.regions:
+        for region in str(args.regions).split(","):
+            main(region)
+    else:
+        main("PT")
